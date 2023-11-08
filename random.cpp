842692413f760242608d2c21c3fdf8a79b5b704a
LoTfree <lotfree618@gmail.com>
2023-11-08 08:57:52 +0800

    change little Cout.

--- random.cpp
+++ random.cpp
@@ -6,6 +6,6 @@ int main() {
     srand(time(NULL)); // 设置随机数种子为当前时间
     int num = 0;
     num = rand() % 90000 + 10000; // 生成10000到99999之间的随机数
-    std::cout << num << std::endl; // 输出随机数
+    std::cout << "一个随机的5位数:" << num << std::endl; // 输出随机数
     return 0;
 }
