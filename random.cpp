366c367606dd1777e9417fc59addf161aae4a864
LoTfree <91266981+Lotfree618@users.noreply.github.com>
2023-11-08 08:40:58 +0800

    Update random.cpp

--- random.cpp
+++ random.cpp
@@ -2,9 +2,12 @@
 #include <cstdlib>
 #include <ctime>
 
+using namespace std;
+
 int main() {
     srand(time(NULL)); // 设置随机数种子为当前时间
-    int num = rand() % 90000 + 10000; // 生成10000到99999之间的随机数
-    std::cout << num << std::endl; // 输出随机数
+    int num = 0;
+    num = rand() % 90000 + 10000; // 生成10000到99999之间的随机数
+    cout << num << endl; // 输出随机数
     return 0;
 }
